7402198886e3273e17c7a74b9bd5665c83903be2
Gabriel Fanti do Nascimento <gabrielfanti1@hotmail.com>
2023-03-02 19:48:00 -0300

    Add files via upload
    Initial commit

---
+++ QuantasVogais/QuantasVogais/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace QuantasVogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma palavra ou frase: ");
+            string entrada = Console.ReadLine();
+            int contagem = 0;
+
+            foreach (char letra in entrada)
+            {
+                if ("aeiouAEIOU".IndexOf(letra) >= 0)
+                {
+                    contagem++;
+                }
+            }
+
+            Console.WriteLine("A quantidade de vogais na palavra ou frase é: " + contagem);
+            Console.ReadKey();
+        }
+    }
+}
